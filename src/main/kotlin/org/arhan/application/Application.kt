25a379965d877a47ed3e34562345d6f83078c55c
Anton Arhipov <ant.arhipov@gmail.com>
2018-11-30 21:58:53 +0200

    feature 002

--- src/main/kotlin/org/arhan/application/Application.kt
+++ src/main/kotlin/org/arhan/application/Application.kt
@@ -7,6 +7,6 @@ import org.springframework.boot.runApplication
 class Application
 
 fun main(args: Array<String>) {
-    println("Start the application!!!1")
+    println("Start the application!!! now!")
     runApplication<Application>(*args)
 }
