7d1fc40b71186b0c53e9437b47c2a96c0082fee8
Anton Arhipov <ant.arhipov@gmail.com>
2018-12-05 19:47:59 +0200

    Update Application.kt

--- src/main/kotlin/org/arhan/application/Application.kt
+++ src/main/kotlin/org/arhan/application/Application.kt
@@ -10,3 +10,14 @@ fun main(args: Array<String>) {
     println("Start the application!")
     runApplication<Application>(*args)
 }
+
+fun a(): String {
+    println("This is a method")
+    return "method"
+}
+
+
+fun b(): String {
+    println("This is a method")
+    return "method"
+}
