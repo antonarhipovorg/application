038bc79908cb5395d7c4e5ea10c1c3ea6764c167
Anton Arhipov <43440971+antonarhipovjb@users.noreply.github.com>
2019-01-14 11:39:28 +0200

    Update Application.kt

--- src/main/kotlin/org/arhan/application/Application.kt
+++ src/main/kotlin/org/arhan/application/Application.kt
@@ -7,7 +7,7 @@ import org.springframework.boot.runApplication
 class Application
 
 fun main(args: Array<String>) {
-    println("Start the application!")
+    println("Start the application!!!!!!!---->")
     runApplication<Application>(*args)
 }
 
