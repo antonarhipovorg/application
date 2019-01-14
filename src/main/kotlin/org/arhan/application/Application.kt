111b251c3763dcfa15de49c1f86ee4ed59b7708a
Anton Arhipov <43440971+antonarhipovjb@users.noreply.github.com>
2019-01-14 15:36:16 +0200

    remove some code

--- src/main/kotlin/org/arhan/application/Application.kt
+++ src/main/kotlin/org/arhan/application/Application.kt
@@ -10,14 +10,3 @@ fun main(args: Array<String>) {
     println("Start the application!!!!")
     runApplication<Application>(*args)
 }
-
-fun a(): String {
-    println("This is a method")
-    return "method"
-}
-
-
-fun b(): String {
-    println("This is a method!!!")
-    return "method"
-}
