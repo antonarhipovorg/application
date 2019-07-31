a5e3c00c814b666080a8961412eb96bca8baa14f
Anton Arhipov <43440971+antonarhipovjb@users.noreply.github.com>
2019-07-31 14:49:25 +0300

    clean up

--- src/main/kotlin/org/arhan/application/Application.kt
+++ src/main/kotlin/org/arhan/application/Application.kt
@@ -10,12 +10,3 @@ fun main(args: Array<String>) {
     println("Start the application!")
     runApplication<Application>(*args)
 }
-
-fun zzz() = "zzz"
-fun yyy() = "yyy"
-fun zzz() = "xxx"
-
-fun b(): String {
-    println("This is a method!!!")
-    return "method"
-}
